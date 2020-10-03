e1965106e1072be60433f34c40c5bad741d973ff
Rajeshroulo <rajraval017@gmail.com>
2020-10-03 17:16:13 +0530

    Added script to search for a product

---
+++ Flipkart/Pages/HomePage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System.Threading;
+
+namespace Flipkart.Pages
+{
+    public class HomePage
+   {
+
+        public IWebDriver driver;
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//input[@placeholder='Search for products, brands and more']")]
+        public IWebElement SearchBox;
+        
+        [FindsBy(How = How.XPath, Using = "//button[@class='vh79eN']//*[local-name()='svg']")]
+        public IWebElement SearchButton;
+
+        [FindsBy(How = How.XPath, Using = "//body/div[@id='container']/div[1]/div[3]/div[2]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/a[1]/div[2]/div[1]/div[1]")]
+        public IWebElement samsungPhone;
+
+        public void SearchMobile()
+        {
+            SearchBox.Click();
+            SearchBox.SendKeys("Samsung Guru Music 2");
+            SearchButton.Click();
+            Thread.Sleep(10000);
+            samsungPhone.Click();
+            Thread.Sleep(7000);
+        }
+
+   }
+}
--- Flipkart/Test/FlipkartTests.cs
+++ Flipkart/Test/FlipkartTests.cs
@@ -6,14 +6,23 @@ using System;
 namespace Flipkart.Test
 {
     public class FlipkartTests : BaseClass
-   {
-        [Test]
-        public void BookswagonLogin()
+    {
+        [Test,Order(1)]
+        public void FlipkartLogin()
         {
             var login = new LoginPage(driver);
             login.AccountLogin();
             string url = "https://www.flipkart.com/";
             Assert.AreEqual(driver.Url, url);
         }
-   }
+
+        [Test, Order(2)]
+        public void SearchProduct()
+        {
+            var product = new HomePage(driver);
+            product.SearchMobile();
+            string title = "Samsung Guru Music 2 - Buy Products Online at Best Price in India - All Categories | Flipkart.com";
+            Assert.AreEqual(driver.Title, title);
+        }
+    }
 }
